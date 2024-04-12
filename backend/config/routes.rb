6c9c40d11aa1a9964dcb417f8191e8cc0d9e4c32
v-lane <victoria.lane.m@gmail.com>
2024-04-12 15:31:40 -0400

    Merge

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :groups
-=======
   resources :member_transactions
   resources :transactions
->>>>>>> 9c6ab09d164cf103a331355babe1c3627275d888
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
