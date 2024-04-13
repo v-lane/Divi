80434721498b990c117d27fc684314d2fd13f4cc
v-lane <victoria.lane.m@gmail.com>
2024-04-13 17:39:54 -0400

    Add styles to sidebar

--- frontend/src/components/SideNavigationBar.jsx
+++ frontend/src/components/SideNavigationBar.jsx
@@ -4,103 +4,147 @@ import '../styles/SideNavigationBar.scss';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
-import Icon from '@mui/material/Icon';
-
-import Typography from '@mui/material/Typography';
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Link from '@mui/material/Link';
 
 const SideNavigationBar = (props) => {
 
   return (
     <aside className="side-nav-bar">
       <header className="accordian-nav">
-        <Accordion>
+        <Accordion defaultExpanded>
           <AccordionSummary
-            expandIcon={<ArrowDownwardIcon />}
+            expandIcon={<ExpandMoreIcon />}
             aria-controls="panel1-content"
-            id="panel1-header"
+            id="username-header"
           >
             <h2>Username</h2>
           </AccordionSummary>
           <AccordionDetails>
             <ul>
-              <li>Dashboard</li>
-              <li>Transactions</li>
-              <li>Groups</li>
-              <li>Notifications</li>
-              <li>
-                <Accordion>
-                  <AccordionSummary
-                    expandIcon={<ArrowDownwardIcon />}
-                    aria-controls="panel2-content"
-                    id="panel2-header"
-                  >
-                    <h3>Transactions</h3>
-                  </AccordionSummary>
-                  <AccordionDetails>
-                    <ul>
-                      <li>Add Expense</li>
-                      <li>Add Payment</li>
-                    </ul>
-                  </AccordionDetails>
-                </Accordion>
+              <li className="active">
+                <Link href="#" underline="none">
+                  Dashboard
+                </Link>
               </li>
               <li>
-                <Accordion>
-                  <AccordionSummary
-                    expandIcon={<ArrowDownwardIcon />}
-                    aria-controls="panel3-content"
-                    id="panel3-header"
-                  >
-                    <h3>Groups</h3>
-                  </AccordionSummary>
-                  <AccordionDetails>
-                    <ul>
-                      <li>Create New Group</li>
-                    </ul>
-                  </AccordionDetails>
-                </Accordion>
+                <Link href="#" underline="none">
+                  Transactions
+                </Link>
+              </li>
+              <li>
+                <Link href="#" underline="none">
+                  Groups
+                </Link>
+              </li>
+              <li>
+                <Link href="#" underline="none">
+                  Notifications
+                </Link>
               </li>
             </ul>
+            <Accordion >
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1-content"
+                id="transactions-header"
+              >
+                <h3>Transactions</h3>
+              </AccordionSummary>
+              <AccordionDetails>
+                <ul>
+                  <li>
+                    <Link href="#" underline="none">
+                      Add Expense
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#" underline="none">
+                      Add Payment
+                    </Link>
+                  </li>
+                </ul>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion >
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel3-content"
+                id="groups-header"
+              >
+                <h3>Groups</h3>
+              </AccordionSummary>
+              <AccordionDetails>
+                <ul>
+                  <li>
+                    <Link href="#" underline="none">
+                      Create New Group
+                    </Link>
+                  </li>
+                </ul>
+              </AccordionDetails>
+            </Accordion>
+
+
           </AccordionDetails>
         </Accordion>
-        <Accordion>
+        <Accordion defaultExpanded>
           <AccordionSummary
-            expandIcon={<ArrowDownwardIcon />}
+            expandIcon={<ExpandMoreIcon />}
             aria-controls="panel4-content"
-            id="panel4-header"
+            id="groupname-header"
           >
             <h2>Group Name</h2>
           </AccordionSummary>
           <AccordionDetails>
             <ul>
-              <li>Transactions</li>
-              <li>Members</li>
               <li>
-                <Accordion>
-                  <AccordionSummary
-                    expandIcon={<ArrowDownwardIcon />}
-                    aria-controls="panel5-content"
-                    id="panel5-header"
-                  >
-                    <h3>Group's Transactions</h3>
-                  </AccordionSummary>
-                  <AccordionDetails>
-                    <ul>
-                      <li>Add Expense</li>
-                      <li>Add Payment</li>
-                    </ul>
-                  </AccordionDetails>
-                </Accordion>
+                <Link href="#" underline="none">
+                  Transactions
+                </Link>
+              </li>
+              <li>
+                <Link href="#" underline="none">
+                  Members
+                </Link>
               </li>
             </ul>
+
+            <Accordion >
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel5-content"
+                id="group-transactions-header"
+              >
+                <h3>Group's Transactions</h3>
+              </AccordionSummary>
+              <AccordionDetails>
+                <ul>
+                  <li>
+                    <Link href="#" underline="none">
+                      Add Expense
+                    </Link>
+                  </li>
+                  <li>
+                    <Link href="#" underline="none">
+                      Add Payment
+                    </Link>
+                  </li>
+                </ul>
+              </AccordionDetails>
+            </Accordion>
+
+
           </AccordionDetails>
         </Accordion>
       </header>
       <footer>
         <ul>
-          <li>My Profile</li>
+          <li>
+            <Link href="#" underline="none">
+              My Profile
+            </Link>
+          </li>
           {/* <li>Admin</li> */}
         </ul>
       </footer>
