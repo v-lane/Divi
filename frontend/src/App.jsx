fbb5ba3f87c8b67ec965b66a6f0f87602d9ee572
v-lane <victoria.lane.m@gmail.com>
2024-04-13 10:23:47 -0400

    Add custom theme with scss variables and react mui theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-import './App.scss'
+import './App.scss';
+import TopNavigationBar from './components/TopNavigationBar';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './styles/createTheme';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h1>Welcome to DIVI</h1>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+        <header>
+          <TopNavigationBar />
+        </header>
+        <body>
+          <h1>Welcome to DIVI</h1>
+        </body>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/styles/createTheme.js
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material/styles';
+import { blueGrey } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary_nav: {
+      light: blueGrey[400],
+      main: blueGrey[500],
+      dark: blueGrey[800],
+      contrastText: blueGrey[50],
+    },
+    // secondary: {
+    //   light: '#ff7961',
+    //   main: '#f44336',
+    //   dark: '#ba000d',
+    //   contrastText: '#000',
+    // },
+  },
+});
+
+export default theme
