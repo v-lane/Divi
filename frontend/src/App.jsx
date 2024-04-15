4cf31302d07c317273afa6bb60362ac6428cfa9a
v-lane <victoria.lane.m@gmail.com>
2024-04-15 09:19:15 -0400

    Add modal view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,18 @@ import React, { useState } from 'react';
 import './App.scss';
 import TopNavigationBar from './components/TopNavigationBar';
 import SideNavigationBar from './components/SideNavigationBar';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from './styles/createTheme';
 import ThreeSectionBody from './components/ThreeSectionBody';
+import ModalView from './components/ModalView';
+import theme from './styles/createTheme';
 
+import { ThemeProvider } from '@mui/material/styles';
 
 function App() {
+  const [modalView, setModalView] = useState(true)
+
+  const handleClick = (() => {
+    setModalView(false);
+  })
 
   return (
     <ThemeProvider theme={theme}>
@@ -18,6 +24,7 @@ function App() {
       <main>
         <SideNavigationBar />
         <ThreeSectionBody />
+        {modalView && <ModalView handleClick={handleClick}/>}
       </main>
     </ThemeProvider>
   );
