cd8b3a9b6d852098e758cf5fe654462c9a79a6ce
v-lane <victoria.lane.m@gmail.com>
2024-04-15 14:03:19 -0400

    Modify profile formatting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useNavigate, useLocation } from "react-router-dom";
-
+import axios from 'axios';
 
 import './App.scss';
 import TopNavigationBar from './components/TopNavigationBar';
@@ -26,31 +26,31 @@ function App() {
     navigate(-1);
   });
 
-  // useEffect(() => {
-  //   axios
-  //   .get("/api/user/:id")
-  //   .then((res) => res.data)
-  //   .then(setUser);
-  // }, [])
+  const userId = 1;
+  useEffect(() => {
+    axios
+    .get(`http://localhost:3001/api/users/${userId}`)
+    .then((res) => res.data)
+    .then(setUser);
+  }, [])
 
 
   return (
     <div className='App'>
       <ThemeProvider theme={theme}>
         <header>
-          <TopNavigationBar location={background || location}/>
+          <TopNavigationBar location={background || location} />
         </header>
         <main>
-          <SideNavigationBar location={background || location}/>
+          <SideNavigationBar location={background || location} />
           <Routes location={background || location}>
-            <Route path='/' element={<ThreeSectionBody/>} >
+            <Route path='/' element={<ThreeSectionBody />} >
               <Route path='profile' element={<ModalView />} />
             </Route>
-            
           </Routes>
           {background && (
             <Routes>
-              <Route path='profile' element={<ModalView handleClick={handleClick} userProfileData={userProfileData} />} />
+              <Route path='profile' element={<ModalView handleClick={handleClick} userProfileData={user} />} />
             </Routes>
           )}
         </main>
