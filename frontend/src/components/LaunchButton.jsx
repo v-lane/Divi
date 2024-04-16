befdbb014df85ab6b497cac79cc66a82bdde0452
Rylan Baun <rylanjbaun@gmail.com>
2024-04-16 13:52:58 -0600

    Fix styling for transaction item

--- frontend/src/components/LaunchButton.jsx
+++ frontend/src/components/LaunchButton.jsx
@@ -5,7 +5,7 @@ import LaunchIcon from '@mui/icons-material/Launch';
 const LaunchButton = (props) => {
 
   return (
-    <Button className="launch-button" variant="outlined" color="primary" size="large"><LaunchIcon /></Button>
+    <Button className="launch-button" variant="outlined" color="primary" size="medium"><LaunchIcon /></Button>
   );
 };
 
