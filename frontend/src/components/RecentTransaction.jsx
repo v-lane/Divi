288af8d69045c225033e1aee0f33acf3e238168a
v-lane <victoria.lane.m@gmail.com>
2024-04-16 09:13:03 -0400

    Fix all bullet shadowbox, scrolling outside container, general formatting

--- frontend/src/components/RecentTransaction.jsx
+++ frontend/src/components/RecentTransaction.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React from 'react';
 
-import '../styles/TransactionItem.scss'
-import '../styles/RecentTransaction.scss'
+import '../styles/TransactionItem.scss';
+import '../styles/RecentTransaction.scss';
 import { Button } from "@mui/material";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import TransactionItem from './TransactionItem';
 
 const RecentTransaction = (props) => {
 
-  
+
 
   return (
-    <>
-    <h1 id='body-title' className='title'>RECENT TRANSACTIONS</h1>
-      <TransactionItem transactionData={props.transactionData}/>
+    <section className='recent-transactions'>
+      <h2 id='body-title' className='title'>Recent Transactions</h2>
+      <TransactionItem transactionData={props.transactionData} />
       <footer className='transactions-footer'>
-        <Button className="transaction-button" variant="contained" color="info">Add Expense</Button>
-        <Button className="transaction-button" variant="contained" color="info">Add Payment</Button>
-        <Button className="transaction-button" variant="contained" color="info"><ArrowForwardIcon/></Button>
+        <div>
+          <Button className="transaction-button-expense" variant="contained" color="error">Add Expense</Button>
+          <Button className="transaction-button-payment" variant="contained" color="success">Add Payment</Button>
+        </div>
+        <Button className="transaction-button-more" variant="contained" color="info"><ArrowForwardIcon /></Button>
       </footer>
-    </>
+    </section>
   );
 };
 
