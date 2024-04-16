89d2d89e817c3b2d7886cb1189caa62ef1dd934e
v-lane <victoria.lane.m@gmail.com>
2024-04-16 17:06:48 -0400

    Add default false to table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_12_203521) do
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.string "group_type"
-    t.boolean "is_archived"
+    t.boolean "is_archived", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
