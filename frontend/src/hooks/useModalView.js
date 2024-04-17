05e409545e11e7c76bc0ef107ba9a3cc85de0007
v-lane <victoria.lane.m@gmail.com>
2024-04-16 20:51:54 -0400

    Add comments

--- frontend/src/hooks/useModalView.js
+++ frontend/src/hooks/useModalView.js
@@ -3,23 +3,25 @@ import React, { useState } from "react";
 export default function useModalView() {
   const [profileView, setProfileView] = useState(false);
   const [newGroupView, setNewGroupView] = useState(false);
+  // add new state here
 
   const closeModal = () => {
     setProfileView(false);
     setNewGroupView(false);
+    // add new set false here
   }
 
   const openModal = (urlpath) => {
     if(urlpath === "profile") setProfileView(true)
     if(urlpath === "new-group") setNewGroupView(true)
+    // add new path here to set true
   }
 
 
   return {
     profileView,
     newGroupView,
-    setProfileView,
-    setNewGroupView,
+    // add new state view here
     closeModal,
     openModal
   };
