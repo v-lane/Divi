d9080426d89ad90e08ada68f313169449d55a2a5
v-lane <victoria.lane.m@gmail.com>
2024-04-16 20:06:58 -0400

    Add users_email route and find by email for create group form

--- frontend/src/components/modals/CreateGroupForm.jsx
+++ frontend/src/components/modals/CreateGroupForm.jsx
@@ -17,6 +17,11 @@ const CreateGroupForm = ({ useModalView }) => {
     memberEmail: ''
   });
 
+  ///////////////////
+  // TEMPORARY - to be replaced by session cookies
+  const userId = 1;
+  /////////////////
+
   const handleChange = (event) => {
     setFormValue({
       ...formValue,
@@ -26,22 +31,70 @@ const CreateGroupForm = ({ useModalView }) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const formData = {
+    const groupData = {
       name: formValue.groupName,
-      group_type: formValue.groupType    
-    }
-    console.log(formData);
-    axios
-      .post(`/api/groups/`, formData)
-      .then((response) => {
-        console.log('post created:', response.data);
-        navigate('/');
-        useModalView.closeModal();
-      })
-      .catch((error) => {
-        console.error("Error creating post:", error);
-      });
+      group_type: formValue.groupType,
+      user_id: userId
+    };
+     // save group
+     axios
+     .post(`/api/groups/`, groupData)
+     .then((response) => {
+       const groupId = response.data.id;
+       const userGroupDataOwner = {
+         user_id: userId,
+         group_id: groupId,
+         is_owner: true
+       };
+
+       //check if member email exists
+       if (formValue.memberEmail) {
+        const emailArr = formValue.memberEmail.split('.')
+        const urlString = `start=${emailArr[0]}&end=${emailArr[1]}`
+        console.log('urlString',urlString);
+
+         axios
+           .get(`/api/users_email/${urlString}`)
+           .then(response => {
+            console.log('response.data', response);
+             if (response.data) {
+               const userGroupDataMember = {
+                 user_id: response.data.id,
+                 group_id: groupId,
+                 is_owner: false
+               };
+               // save group member to UserGroups
+               axios
+                 .post(`/api/user_groups`, userGroupDataMember)
+                 .then(response => {
+                   console.log('User Member record created. Notification record needs to be created');
+                   ///////////////////////
+                   //ADD LOGIC HERE TO CREATE NOTIFICATION FOR GROUP MEMBER
+                   ///////////////////////
+                 })
+             } else {
+               console.log('User does not exist. Notification email needs to be created');
+               //////////////////
+               //ADD LOGIC HERE TO CREATE NOTIFICATION (EMAIL)
+               //////////////////
+             }
+           });
+       };
+
+       // save owner to UserGroups
+       axios
+         .post(`/api/user_groups`, userGroupDataOwner)
+         .then((response) => {
+           console.log('post created:', response.data);
+           navigate('/');
+           useModalView.closeModal();
+         });
+     })
+     .catch((error) => {
+       console.error("Error creating post:", error);
+     });
   };
+
 
 
   return (
