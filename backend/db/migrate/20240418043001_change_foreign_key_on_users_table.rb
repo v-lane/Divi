4f897a47e01ff0c8715b9ea54f530c3ee651f604
Rylan Baun <rylanjbaun@gmail.com>
2024-04-17 23:16:40 -0600

    Add final delete user styling

--- backend/db/migrate/20240418043001_change_foreign_key_on_users_table.rb
+++ backend/db/migrate/20240418043001_change_foreign_key_on_users_table.rb
@@ -1,6 +1,6 @@
 class ChangeForeignKeyOnUsersTable < ActiveRecord::Migration[7.0]
   def change
-    remove_foreign_key :user_groups, :user_id
-    add_foreign_key :user_groups, :user_id, on_delete: cascade
+    remove_foreign_key :user_groups, :users
+    add_foreign_key :user_groups, :users, on_delete: :cascade
   end
 end
