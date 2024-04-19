c8129487f3c2661efd9068519cf5f433e4320f51
Rylan Baun <rylanjbaun@gmail.com>
2024-04-19 16:44:37 -0600

    Add group transaction chart

--- frontend/src/routes/ThreeSectionBody.jsx
+++ frontend/src/routes/ThreeSectionBody.jsx
@@ -4,6 +4,7 @@ import '../styles/ThreeSectionBody.scss';
 import RecentTransaction from '../components/RecentTransaction';
 import Groups from '../components/Groups/Groups';
 import TransactionChart from '../components/TransactionChart';
+import GroupTransactionChart from '../components/GroupTransactionChart'
 import Members from '../components/Members/Members';
 
 import { useLocation } from 'react-router-dom';
@@ -19,6 +20,7 @@ const ThreeSectionBody = (props) => {
       <div className='left'>
         <article className='top'>
           {location.pathname === '/' && <TransactionChart transactionData={transactionData} memberTransactions={memberTransactions} user={user} group={userGroups} />}
+          {activeGroup > 0 && <GroupTransactionChart transactionData={transactionData} memberTransactions={memberTransactions} user={user} group={userGroups} activeGroup={activeGroup} activeGroupDetails={activeGroupDetails}/>}
         </article>
         <article className='bottom'>
           {location.pathname === '/' && <RecentTransaction transactionData={transactionData} openModal={openModal} setActiveTransaction={setActiveTransaction}/>}
