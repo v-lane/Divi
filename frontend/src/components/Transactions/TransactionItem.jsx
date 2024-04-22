55ee21182596f7356a752987bc1101d6ff653ea9
v-lane <victoria.lane.m@gmail.com>
2024-04-22 12:25:25 -0400

    Update transaction item with 'sent to' bullet

--- frontend/src/components/Transactions/TransactionItem.jsx
+++ frontend/src/components/Transactions/TransactionItem.jsx
@@ -1,19 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { format } from 'date-fns';
-import { Link, useLocation } from "react-router-dom";
-import { Button } from '@mui/material';
-import LaunchIcon from '@mui/icons-material/Launch';
 import LaunchButton from '../LaunchButton';
 import '../../styles/TransactionItem.scss';
-import axios from 'axios';
-
-
 
 const TransactionItem = (props) => {
   const { openModal, setActiveTransaction, transaction, group, user, amount } = props;
-  const location = useLocation();
-
-  const [username, setUsername] = useState(null);
 
   const { } = props;
 
@@ -26,14 +17,6 @@ const TransactionItem = (props) => {
     setActiveTransaction(transactionId);
   };
 
-  // if (transaction.recipient_id) {
-  //   axios
-  //     .get(`/api/transaction_by_id/${transaction.id}`)
-  //     .then((res) => {
-  //       setUsername(res.data);
-  //     });
-  // }
-
   return (
     <li className='transaction'>
       {group && <p className='transaction-group' onClick={(() => launchTransaction(transaction.id))} >Group: {group.name} <LaunchButton goTo={`transaction-details/${transaction.id}`} color={0} openModal={openModal} /></p>}
@@ -45,10 +28,11 @@ const TransactionItem = (props) => {
       </p>
       <p className='sent-posted-heading'>
         Sent To:
-        {/* {transaction.recipient_id === null ?
-          <> All Group Members</> :
-          // <> Individual { username !== null && username.username}</>
-        } */}
+        {transaction.recipient_id === null ?
+          <> All Group Members </> :
+          <> Individual </>
+        } 
+        (Click top right for more details)
       </p>
       <p className='transaction-date'><span>Transaction Date: {format(new Date(transaction.transaction_date), 'MMMM dd, yyyy')} </span><span className={transaction.transaction_type}>{transaction.transaction_type}</span></p>
     </li>
